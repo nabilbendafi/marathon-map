2e04daa8622982117c34429f4464004e843944f8
Nabil Bendafi <nabil@bendafi.fr>
2017-10-22 22:12:18 +0200

    Integrate jVectorMap

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-
+$(function(){
+  $.getJSON('data.json', function(data){
+    var marathons = data.races.marathons;
+    var count_marathons = _.countBy(marathons, 'country_code2');
+    $('#world-map').vectorMap({
+      map: 'world_mill',
+      series: {
+        regions: [{
+          values: count_marathons,
+          scale: ['#C8EE00', '#0071A4'],
+          min: 0,
+          attribute: 'fill'
+        }]
+      },
+      onRegionTipShow: function(e, el, code){
+        if (count_marathons[code])
+          el.html(el.html() + ': ' + count_marathons[code]);
+      }
+    });
+  });
+});
